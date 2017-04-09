ac28b39dc7cf11a72abf5e6450bed505dca7b7dc
leventel8 <leventel8@yandex.ru>
2017-04-09 19:35:45 +0300

    change MainFrom

--- CarApp/MainForm.cs
+++ CarApp/MainForm.cs
@@ -49,6 +49,7 @@ namespace CarApp
             db.SaveChanges();
 
             MessageBox.Show("Новый объект добавлен");
+            MessageBox.Show("Новый объект добавлен");
         }
 
         // кнопка редактирования объекта Car на главной форме
